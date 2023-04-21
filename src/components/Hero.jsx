a1c7c764757abfdf28f176c297b35071cc42dc0a
AugustoLa <augusto_florindo@hotmail.com>
2023-04-21 14:13:38 -0300

    lading page added - building

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div className='text-white'>
+            <div className='max-w-[800px] mx-auto text-center justify-center flex flex-col mt-[-96px] w-full h-screen'>
+                <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
+                <h1 className='md:text7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
+                <div>
+                    <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing for</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
