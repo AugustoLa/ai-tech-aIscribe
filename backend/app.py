107f6a0377482b8b7acadbaa3a936cc3381578c9
AugustoLae <augusto_florindo@hotmail.com>
2023-06-20 20:31:10 -0300

    api connected to chatui

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,32 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+import openai
+from dotenv import load_dotenv, find_dotenv
 
+# DOTENV
+load_dotenv(find_dotenv())
+openai.api_key = os.getenv("OPEN_AI_KEY")
+
+# FLASK
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/")
-def index():
-    return "Hello, Flask!"
-
-
-@app.route("/api/data", methods=["POST"])
-def process_data():
-    data = request.json
-    # Processar os dados recebidos
-    response = {"message": "Data processed successfully"}
-    return jsonify(response)
+@app.route("/api", methods=["POST"])
+def gpt3():
+    data = request.get_json()
+    message = data["message"]
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=3000,
+        temperature=0.9,
+    )
+    print(response.choices[0].text)
+    return jsonify({'message': response.choices[0].text})
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=9000, debug=True)
--- frontend/src/Routes/Auth/Dashboard/views/AiFriend.jsx
+++ frontend/src/Routes/Auth/Dashboard/views/AiFriend.jsx
@@ -1,45 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios'
 import Friend from '../../../../assets/img/aifriend.png';
 
 function AiFriend() {
-    const [messages, setMessages] = useState([
-        { type: 'answer', text: "Hi, my name's Lorem, how can I help you?" },
-        { type: 'question', text: "Who is Elon Musk" }
-    ]);
+    //States
+    const [inputMsg, setInputMsg] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [chatMsg, setChatMsg] = useState([{
+        user: 'bot',
+        message: 'How can i help you?'
+    }])
 
-    const handleInputChange = (event) => {
-        if (event.key === 'Enter') {
-            const inputValue = event.target.value;
-            const newMessage = { type: 'question', text: inputValue };
-
-            setMessages([...messages, newMessage]);
-            event.target.value = '';
+    const url = "http://localhost:9000/api"
+    //HandleSubmit
+    const handleSubmit = async () => {
+        setLoading(true)
+        setInputMsg('');
+        let newMessage = [...chatMsg, { user: 'me', message: `${inputMsg}` }];
+        setChatMsg(newMessage)
+        const message = newMessage.map((msg) => msg.message).join('\n')
+        try {
+            const { data } = await axios.post(url, {
+                message: message
+            })
+            setChatMsg([...newMessage, { user: 'bot', message: `${data.message}` }])
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
         }
-    };
+    }
+    //onKeyDown
+    const onKeyDown = (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault()
+            e.stopPropagation()
+            handleSubmit()
+        }
+    }
 
+    //clear chat 
+
+    const clearChat = () => {
+        setChatMsg([])
+    }
+
+    //HandleScrolling
     return (
         <div className='flex flex-col justify-center items-center h-full md:border-2 border-black'>
-            <div className='w-full flex flex-col justify-center max-w-[1200px] rounded-lg m-4 p-4 overflow-y-auto'>
-                <img
-                    src={Friend}
-                    className='rounded-full my-4 mx-auto h-40 w-40 mb-5'
-                />
-                {messages.map((message, index) => (
-                    <div
-                        key={index}
-                        className={`${message.type === 'question'
-                            ? 'text-end bg-gray-200 rounded-md'
-                            : 'text-start'
-                            } ${message.type === 'answer' ? 'bg-blue-200 rounded-md' : ''} w-full m-2 mx-auto p-2 text-gray-700 font-semibold text-xl`}
-                    >
-                        {message.text}
-                    </div>
-                ))}
-            </div>
-            <input
-                className='xl:max-w-[1150px] lg:max-w-[800px] md:max-w-[500px] w-full mx-auto my-3 p-4 rounded-xl bg-gray-100 shadow-2xl'
-                onKeyDown={handleInputChange}
+            <img
+                src={Friend}
+                className='rounded-full my-4 mx-auto h-40 w-40 mb-5'
             />
+            <div className='overflow-y-auto max-w-[1150px] w-full'>
+                {chatMsg.map((message, index) => {
+                    return (
+                        <div className={`flex flex-col w-full my-2 max-w-[1150px] ${message.user === 'me' ? 'items-end' : 'items-start'}`}>
+                            <div key={index}>
+                                {message.user === 'me' ? (
+                                    <div className='bg-gray-200 rounded-md m-2 mx-auto p-2 text-gray-700 font-semibold text-xl'>
+                                        {message.message}
+                                    </div>
+                                ) : (
+                                    <div className='bg-blue-200 rounded-md m-2 mx-auto p-2 text-gray-700 font-semibold text-xl'>
+                                        {message.message}
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+
+
+            <div className='max-w-[1200px] w-full flex flex-row items-center justify-center'>
+                <a onClick={clearChat} className='m-2 bg-blue-800 rounded-lg cursor-pointer p-4'>Clear</a>
+                <input
+                    className='xl:max-w-[1150px] lg:max-w-[800px] md:max-w-[500px] w-full mx-auto my-3 p-4 rounded-xl bg-gray-100 shadow-2xl'
+                    value={inputMsg} onChange={(e) => setInputMsg(e.target.value)}
+                    onKeyDown={onKeyDown}
+                />
+                {
+                    loading ? 'GPT Typing...' :
+                        <a onClick={handleSubmit} className='m-2 bg-blue-800 rounded-lg cursor-pointer p-4'>Send</a>
+                }
+
+            </div>
         </div>
     );
 }
